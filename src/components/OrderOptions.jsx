884fdb9fd9728993d3f517b5fc44000b5f74d1c7
Furkan Aydemir <sem_203@hotmail.com>
2026-03-10 20:36:17 +0300

    Sipariş Detayları Kısmı Tamamlandı

--- src/components/OrderOptions.jsx
+++ src/components/OrderOptions.jsx
@@ -1,28 +1,71 @@
 import React, { useEffect, useState } from 'react'
 import '../css/OrderOptions.css'
+import { toast } from 'sonner'
+import { useNavigate } from 'react-router-dom'
 function OrderOptions() {
-
+    const navigate = useNavigate()
     const [size, setSize] = useState("")
     const [dough, setDough] = useState("")
-    const [ingredients,setIngredients] = useState([])
+    const [ingredientState, setIngredientState] = useState([])
+    const [note, setNote] = useState("")
+    const [count, setCount] = useState(1)
+   
+    
+   const pizza = count*85.50
+
+    const increment = (e) => {
+        e.preventDefault()
+        setCount(count => count + 1)
+    }
+    const decrement = (e) => {
+        e.preventDefault()
+        setCount(count => count - 1)
+    }
+
+
+    const handleNoteChange = (e) => {
+        setNote(e.target.value)
+    }
     const handleSizeChange = (e) => {
         setSize(e.target.value)
     }
-    const handleDoughChange = (e) =>{
+    const handleDoughChange = (e) => {
         setDough(e.target.value)
     }
-   const handleIngredient = (e)=>{
-      setIngredients(e.target.value)
-      
-   }
-    useEffect(()=>{
-     console.log(dough,size)
-    },[dough,size])
+    const handleIngredientChange = (e) => {
+        const value = e.target.value
+        if (ingredientState.includes(value)) {
+            setIngredientState(ingredientState.filter(i => i !== value))
+        } else {
+            if (ingredientState.length == 9) {
+                toast.warning("En fazla 10 malzeme seçebilirsin")
+
+            }
+            setIngredientState([...ingredientState, value])
+        }
+    }
+
+    const ingredients = ["Pepperoni", "Sosis", "Kanada Jambonu", "Tavuk Izgara", "Domates", "Mısır", "Mantar", "Jalepeno", "Sarımsak", "Biber", "Turşu", "Ananas", "Kabak", "Pastırma"]
+    
+     const handleSubmit = (e) =>{
+        e.preventDefault()
+        if(size===""){
+            toast.error("Lütfen Boyut Seçimi Yapınız!")
+            return
+        }else{
+            if(dough===""){
+                toast.error("Lütfen Hamur Seçimi Yapınız!")
+                return
+            }
+        }
+        navigate('/SuccessOrder')
+     }
+
     return (
-        <form className='order-form'>
+        <form onSubmit={handleSubmit} className='order-form'>
             <fieldset className='size-selector'>
                 <legend className='size-title'>Boyut Seç
-                  <span className='required'>*</span>
+                    <span className='required'>*</span>
                 </legend>
                 <div className='radio-group'>
                     <label>
@@ -49,60 +92,46 @@ function OrderOptions() {
             <fieldset className='material-selector'>
                 <div className='material-selection-title'>
                     <h4>Ek Malzemeler</h4>
-                    <p>En fazla 10 malzeme seçebilirsiniz. 5₺</p>
-                </div>
-                <div className='material-selection'>
-                    <div className='flex-column'>
-                        {/* Bu kısım düzeltilecek malzeme içeriği array haline getirilip maple döneceğim. */}
-                        <label>
-                            <input type="checkbox" value={ingredients} onChange={handleIngredient} /><span>Pepperoni</span>
-                        </label>
-                        <label>
-                            <input type="checkbox" /><span>Tavuk Izgara</span>
-                        </label> <label>
-                            <input type="checkbox" /><span>Mısır</span>
-                        </label> <label>
-                            <input type="checkbox" /><span>Sarımsak</span>
-                        </label> <label>
-                            <input type="checkbox" /><span>Ananas</span>
-                        </label>
-
-                    </div>
-                    <div className='flex-column'>
-                        <label>
-                            <input type="checkbox" /><span>Sosis</span>
-                        </label>
-                        <label>
-                            <input type="checkbox" /><span>Soğan</span>
-                        </label> <label>
-                            <input type="checkbox" /><span>Sucuk</span>
-                        </label> <label>
-                            <input type="checkbox" /><span>Biber</span>
-                        </label> <label>
-                            <input type="checkbox" /><span>Kabak</span>
-                        </label>
-
-                    </div>
-                    <div className='flex-column'>
-                        <label>
-                            <input type="checkbox" /><span>Kanada Jambonu</span>
-                        </label>
-                        <label>
-                            <input type="checkbox" /><span>Pastırma</span>
-                        </label>
-                        <label>
-                            <input type="checkbox" /><span>Jalepeno</span>
-                        </label>
-                        <label>
-                            <input type="checkbox" /><span>Mantar</span>
-                        </label>
-          
+                    <p style={{color:"grey"}}>En fazla 10 malzeme seçebilirsiniz. 5₺</p>
+                    <div className='checkbox-container'>
+                        {
+                            ingredients.map((ingredient) => (
+                                <label className='checkbox-item' key={ingredient}>
+                                    <input disabled={ingredientState.length >= 10 && !ingredientState.includes(ingredient)} onClick={handleIngredientChange} className='material' type='checkbox' value={ingredient} /><span>{ingredient}</span>
+                                </label>
+                            ))
+                        }
                     </div>
                 </div>
             </fieldset>
             <fieldset className='order-note'>
-               
+                <h4>Sipariş Notu</h4>
+                <input type="text" value={note} onChange={handleNoteChange} className='order-input' placeholder='Siparişinize eklemek istediğiniz bir not var mı?' />
             </fieldset>
+            <fieldset className='order-detail'>
+                <div className='pizza-count'>
+                    <button onClick={decrement} className='count-button'>-</button>
+                    <p className='count'>{count}</p>
+                    <button onClick={increment} className='count-button'>+</button>
+                </div>
+             <div>
+                <div className="order-container">
+                    <h3>Sipariş Detayı</h3>
+
+                    <div className="row">
+                        <span>Seçimler</span>
+                        <span>{ingredientState.length*5}.00₺</span>
+                    </div>
+
+                    <div className="row sum">
+                        <span>Toplam</span>
+                        <span>{pizza+(ingredientState.length*5)}₺</span>
+                    </div>
+                </div>
+                <button className='order-button' type='submit' style={{ width: "100%" }}>SİPARİŞ VER</button>
+                </div>
+            </fieldset>
+            
         </form>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import AppRoutes from './routes/AppRoutes.jsx'
+import { Toaster } from 'sonner'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
+  <Toaster position='top-right' richColors/>
     <AppRoutes />
   </>
 )
